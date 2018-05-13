0c03ddef658667a1b2bdfe3d306bf74609beb717
thomaskeady <thomas.keady@jhu.edu>
2018-05-12 23:59:38 -0400

    This one should work

--- cedict_data_preprocess.py
+++ cedict_data_preprocess.py
@@ -3,10 +3,9 @@ import string
 
 ### Rules for separating
 # Pick the first definition (separated by '/')
-# Of those, remove anything in parenthesis
-#	(unless only thing is in parenthesis, in which case strip parenthesis)
-# then take longest word (is this the best move?)
-# 	Look into phrase2vec
+# Turn - into _
+# Remove parenthesis, and other punctuation
+# Pick longest word
 
 
 
@@ -15,7 +14,7 @@ with open('data/raw/cedict_ts_u8.txt', 'r') as data:
 	count = 0
 	total = 0
 
-	outFile = open('data/processed/cedict_processed.txt', 'w')
+	outFile = open('data/raw/cedict_only_singles.txt', 'w')
 
 	for line in data:
 		total = total + 1
@@ -25,84 +24,27 @@ with open('data/raw/cedict_ts_u8.txt', 'r') as data:
 		if len(line[0]) == 1:
 
 			i = 1
-			while(i < len(line)):
+			while(i < len(line)):	# To skip to definition part
 				if ']' in line[i]:
 					count = count + 1
-					#print(total)
-					#print(line[i])
-					#print(' '.join(line[i+1:len(line)]))
 					chinese = line[0]
 					#english = ' '.join(line[i+1:len(line)]).split('/')
 					english = ' '.join(line[i+1:len(line)])
-					#english = english.split('/')[1]	# Take first definition only
-					#if "(" in english:
-					#	print(chinese + '\t' + str(english))
-					#print(chinese + '\t' + str(english))
 
 					english = english.split('/')
 
-					# #print(chinese + '\t' + str(english[1]))
-					# print(english[1])
-					#
-					# regex = re.compile(".*?\((.*?)\)")
-					# result = re.findall(regex, english[1])
-					# print(result)
-					#
-					# subbed = re.sub(regex, '', english[1])
-					# print(subbed)
-					#
+					english = re.sub('-', '_', english[1])
+					english = re.sub('[^a-zA-Z ]', '', english)
+					#print(english)
+					words = english.split()
 
+					longest = max(words, key=len)
 
-
-					#onlyLetters = ' '.join(result.concatenate(subbed))
-					#print(onlyLetters)
-
-					words = english[1].split()	# Take the first definition
-					#print(chinese + '\t' + str(words))
-
-					longest = ''
-
-					# # Delete parenthesis if theres more than one word
-					# if len(words) > 1:
-					# 	toRemove = []
-					#
-					# 	for i in range(len(words)-1):
-					# 		if '(' in words[i] or ')' in words[i]:
-					# 			#words.remove(words[i])
-					# 			toRemove.append(i)
-					#
-					# 		words[i] = re.sub(r'\W', '', words[i])
-					# 		#print(words[i])
-					#
-					# 	for r in toRemove:
-					# 		words.remove(words[r])
-					#
-					#
-					# 	#print(str(words))
-					# 	longest = max(words, key=len)	# This takes the first if multiple with same length
-					# 	#print(longest)
-					#
-					# else:	# If just one, remove parenthesis if they exist
-					# 	if '(' in words[0] or ')' in words[0]:
-					# 		longest = words[0][1:len(words[0])-1]
-					# 	else:
-					# 		longest = words[0]
-					#
-					# 	longest = re.sub(r'\W', '', longest)
-					# 	#print(longest)
-
-					
-
-
-
-					#longest = max(words, key=len)	# This takes the first if multiple with same length
-
-					#print(str(words) + ' ' + longest)
-					#print(longest)
+					print(chinese + ' ' + longest)
 
 					#outFile.write(chinese + ' ' + longest + '\n')
 					#print(chinese + ' ' + str(words) + ' ' + longest)
-					print(chinese + ' ' + longest)
+					#print(chinese + ' ' + longest)
 
 					# Must be last thing in the if statement
 					i = len(line)
